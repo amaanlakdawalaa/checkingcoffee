81991b00476b4c5cac1aea1d6bc6519bede4617a
amaanlakdawalaa <114981484+amaanlakdawalaa@users.noreply.github.com>
2023-01-30 20:35:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+ let carts = document.querySelectorAll('.button');
+
+console.log(carts)
+   for ( let i=0 ; i < carts.length ; i++){
+     carts[i].addEventListener('click', cartsNumber()
+   );
+   }
+
+
+function cartsNumber() {
+  alert('button clicked')
+   let productNumbers = localStorage.getItem("number");
+   productNumbers = parseInt(productNumbers);
+   if (productNumbers){
+   localStorage.setItem("number", productNumbers + 1);
+   document.querySelector('.contact-list span').textContent =  productNumbers + 1 ;
+ } else{
+   localStorage.setItem("number",  1);
+   document.querySelector('.contact-list span').textContent = 1;
+ }
+}
